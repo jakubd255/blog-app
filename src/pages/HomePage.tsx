215af4c5ce8ddff30987718989d7321ef0ed9951
jakubd255 <jakubdudek255@gmail.com>
2024-08-15 21:25:08 +0200

    Infinite scrolling

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -3,6 +3,7 @@ import {APP_NAME} from "@/constants";
 import server from "@/constants/server";
 import {PostSummary} from "@/types";
 import {useEffect, useState} from "react";
+import InfiniteScroll from "react-infinite-scroll-component";
 
 
 
@@ -11,27 +12,44 @@ const HomePage: React.FC = () => {
 
     const [posts, setPosts] = useState<PostSummary[]>([]);
     const [isLoaded, setLoaded] = useState<boolean>(false);
+    const [page, setPage] = useState<number>(1);
+    const [hasMore, setHasMore] = useState<boolean>(true);
 
     useEffect(() => {
         server.get("/api/posts").then(response => {
             setPosts(response.data.content);
+            setHasMore(response.data.totalPages != 1);
             setLoaded(true);
         })
     }, []);
 
+    const handleLoadMore = () => {
+        server.get("/api/posts", {params: {page: page}}).then(response => {
+            setPosts(posts => [...posts, ...response.data.content]);
+            setHasMore(response.data.totalPages == page+1)
+            setPage(page => page+1);
+        });
+    }
     
     if(isLoaded) return(
         <div className="flex flex-col gap-5 w-full">
             <h2 className="text-[35px]">
                 Posts
             </h2>
-            <ul className="flex flex-col gap-5">
-                {posts.length ? posts.map(post => (
-                    <PostListElement post={post}/>
-                )) : (
-                    "There's no posts yet."
-                )}
-            </ul>
+            <InfiniteScroll
+                hasMore={hasMore}
+                dataLength={posts.length || 0}
+                next={handleLoadMore}
+                loader={<h4>Loading...</h4>}
+            >
+                <ul className="flex flex-col gap-5">
+                    {posts.length ? posts.map(post => (
+                        <PostListElement post={post}/>
+                    )) : (
+                        "There's no posts yet."
+                    )}
+                </ul>
+            </InfiniteScroll>
         </div>
     );
 }
